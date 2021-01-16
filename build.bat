82342acd7a5b245985d188fd8ed5eaa77e63ef4f
Yoav Steinberg <mefuga.yt@gmail.com>
2021-01-16 15:47:08 +0200

    Fix build.bat ; add TD.TR to gitignore

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ path=%path%;c:\tasm\bin
 
 tasm.exe /zi /c src\line.asm, obj\line.obj
 tasm.exe /zi /c src\rect.asm, obj\rect.obj
-tasm.exe /zi /c src\main.asm, obj\main.asm
+tasm.exe /zi /c src\main.asm, obj\main.obj
 
 tlink.exe /v obj\line.obj obj\rect.obj obj\main.obj, game.exe
